4f37c01b7a80b4bf051fc03adaaae1063bc1fc83
kobe <kobelee776@gmail.com>
2019-12-18 18:41:39 +0800

    url_sa.lua

--- url_sa.lua
+++ url_sa.lua
@@ -2,8 +2,8 @@ local ret = {};
 ret.bFlag = true;
 ret.content = [[{
 	"serverlist":[
-	"https://aaa.com",
-	"https://bbb.com"
+	    "https://aaa.com",
+	    "https://bbb.com"
 	]
 }]]
 return ret; 
