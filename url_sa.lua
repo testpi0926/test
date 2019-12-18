1c542f8d3469132c802426ebfd42d2992ac8a42d
kobe <kobelee776@gmail.com>
2019-12-18 20:29:12 +0800

    test

--- url_sa.lua
+++ url_sa.lua
@@ -1,8 +1,23 @@
+local cidlist = {666888};
+
 local ret = {};
-ret.bFlag = true;
+ret.bFlag = false;
+
+local localCID = _CIDHandler:getChannelID();
+if cidlist ==nil then
+    ret.bFlag = true;
+else
+    for i,v in ipairs(cidlist) do
+        if localCID == v then
+            ret.bFlag = true;
+        end
+    end
+end
+
 ret.content = [[{
 	"serverlist":[
 	    "https://bbb.com"
 	]
 }]]
-return ret; 
+return ret;
+
